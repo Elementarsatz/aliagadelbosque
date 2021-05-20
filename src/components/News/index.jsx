968ab6c525b72a3315d3bfc68e75d69f1712109a
Elementarsatz <jcaliagadb@hotmail.com>
2021-05-20 05:46:19 -0500

    tenemos news, yay

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {Fragment}  from 'react';
+import {news} from '../../constants/news'
 
 const News = () => (
-    <h1>News</h1>
+    <Fragment>
+        <h1>News</h1>
+        {news.map(item =>  (
+            <Fragment>
+                <h3>{item.title}</h3>
+                {item.body.split("\n").map((meti,windex) => (
+                    
+
+                        <p>{(windex == 0) && `${item.date}: `}{meti.match(/[^\]]+(?![^<]*\])/g)}<a target="_blank" href={meti.match(/(?<=\[)(.*?)(?=\])/g)}>{meti.match(/(?<=\<)(.*?)(?=\>)/g)}</a></p>
+
+                
+                ))}
+
+                    
+            </Fragment>
+        ))}
+        </Fragment>
+
+    
 );
 
 export default News;
